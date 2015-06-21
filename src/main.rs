dfc0978b4cbe48e5315a9c0c66e3bd5d99a1c1ba
Friedrich von Never <friedrich@fornever.me>
2015-06-21 19:01:11 +0600

    Add some initial WinAPI usage code.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
+extern crate kernel32;
+extern crate user32;
+extern crate winapi;
+
+use std::ptr;
+use std::ffi::OsStr;
+use std::os::windows::ffi::OsStrExt;
+
 fn main() {
-    println!("Hello, world!");
+    unsafe { messageLoop(); }
+}
+
+fn to_wchar(str : &str) -> Vec<u16> {
+    OsStr::new(str).encode_wide(). chain(Some(0).into_iter()).collect()
+}
+
+unsafe fn messageLoop() -> i32 {
+    let instance = kernel32::GetModuleHandleW(ptr::null());
+    let class = winapi::WNDCLASSW {
+        style: 0,
+        lpfnWndProc: Some(wndProc),
+        cbClsExtra: 0,
+        cbWndExtra: 0,
+        hInstance: instance,
+        hIcon: ptr::null_mut(),
+        hCursor: ptr::null_mut(),
+        hbrBackground: ptr::null_mut(),
+        lpszMenuName: ptr::null_mut(),
+        lpszClassName: to_wchar("HiddenWindowClass").as_ptr()
+    };
+
+    0
+}
+
+unsafe extern "system" fn wndProc(
+    window: winapi::HWND,
+    message: winapi::UINT,
+    wParam: winapi::WPARAM,
+    lParam: winapi::LPARAM) -> winapi::LRESULT {
+    println!("Message received: {}", message);
+    0
 }
